4144f152292f52c16438a11b37b054538faf87ee
Cameron Eldridge <cameldridge+git@gmail.com>
2019-01-16 22:57:03 -0500

    Add some notes

--- Pigeon/Pigeon/ECS/Component.swift
+++ Pigeon/Pigeon/ECS/Component.swift
@@ -10,13 +10,26 @@ import Foundation
 
 /// A component that is associated with an entity in the entity-component-system.
 ///
-/// TODO: consider versioned components, with a failable upward migration to allow as much inter-version communication
-/// as possible (though changing components should be avoided when possible, as each is so simple there should be
-/// nothing worth changing)
+/// TODO: consider versioned components
+/// *   failable upward migration to allow as much inter-version as possible?
+/// *   changing implementation of existing components should probably be avoided when possible, but being able to
+///     migrate existing components is preferable to having legacy code lying around in later versions
 ///
 /// TODO: figure out how to store, retrieve, and manage expired components
+/// *   can components expire?
+/// *   what if memory is running out, which get removed?
 ///
 /// TODO: figure out how to distribute components
+/// *   how can an entity and its components be serialized and transmitted?
+///
+/// TODO: consider the case of multiple components of the same type for one entity
+/// *   how do these get identified, ordered, and deduped?
+/// *   why not use an array inside a single component?
+///
+/// TODO: how to identify components
+/// *   do components need identifiers (a Token on each component)?
+/// *   do components a reference back to the owning entity?
+/// *   handle this externally to the component (in the DB)?
 protocol Component: Serializable, Deserializable {
     /// The name of this component. Must be unique as it is used to identify the component in the storage
     static var name: String { get }
